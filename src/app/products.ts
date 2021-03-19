16ce8fe73a8e06317e670befb939a421716d21bb
shaimukhametova <shaimukhametova.z@icloud.com>
2021-03-20 00:17:25 +0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    name: "Подарок №1",
+    price: 799,
+    count: 1,
+    image:
+      "https://kazan.dreambox-shop.ru/upload/iblock/259/25957ac7cc211020779942037cccd184.png",
+    description:
+      "В набор входят: кашемировый шарф, авторская ароматизированная свеча, молочный шоколад, подарочная упаковка+оформление"
+  },
+  {
+    name: "Подарок №2",
+    price: 699,
+    count: 1,
+    image:
+      "https://kazan.dreambox-shop.ru/upload/iblock/259/25957ac7cc211020779942037cccd184.png",
+    description:
+      "Плетеная корзина из ассорти бельгийского шоколада, шоколада Ferrero,merci и конфет"
+  },
+  {
+    name: "Подарок №3",
+    price: 299,
+    count: 1,
+    image:
+      "https://kazan.dreambox-shop.ru/upload/iblock/259/25957ac7cc211020779942037cccd184.png",
+    description: "Верх, верхняя деталь гардероба или одежды"
+  },
+  {
+    name: "Подарок №3",
+    price: 299,
+    count: 1,
+    image:
+      "https://kazan.dreambox-shop.ru/upload/iblock/259/25957ac7cc211020779942037cccd184.png",
+    description: "Верх, верхняя деталь гардероба или одежды"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
